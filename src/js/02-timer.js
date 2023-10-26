99d60eab71f43b06e66b36275652f73e0f9cb2ce
TokarevArtemV <tokarev.artem.v@gmail.com>
2023-10-26 23:53:02 +0300

    fix task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -16,81 +16,101 @@ const optionsToFlatpickr = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    timerData = new Date(selectedDates[0]).getTime();
-    changeTimer(timerData);
+    time = new Date(selectedDates[0]).getTime();
+    clockFace.setTimer(time);
   },
 };
 
-
 class ClockFace {
-    
+  static convertMsToTime(milliseconds) {
+    let seconds = Math.floor(milliseconds / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    let day = Math.floor(hours / 24);
+
+    seconds = ClockFace.padTo2Digits(seconds % 60);
+    minutes = ClockFace.padTo2Digits(minutes % 60);
+    hours = ClockFace.padTo2Digits(hours % 24);
+    day = ClockFace.padTo2Digits(day);
+
+    return [day, hours, minutes, seconds];
+  }
+  static padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+  }
+
+  intervalId;
+  timerData;
+  timerIsActive = false;
+
+  constructor(render, startBtn) {
+    this.render = render;
+    this.startBtn = startBtn;
+  }
+
+  disableBtnState(btn) {
+    btn.style.pointerEvents = 'none';
+    btn.style.color = 'grey';
+  }
+  enableBtnState(btn) {
+    btn.style.pointerEvents = 'auto';
+    btn.style.color = 'black';
+  }
+
+  start() {
+    if (this.timerIsActive) {
+      return;
+    }
+    this.intervalId = setInterval(() => {
+      this.changeTimer(this.timerData);
+    }, 1000);
+    this.timerIsActive = true;
+  }
+
+  setTimer(time) {
+    this.timerData = time;
+    if (time <= Date.now()) {
+      this.disableBtnState(this.startBtn);
+      if (!this.timerIsActive) {
+        Notify.info('Please choose a date in the future');
+        this.render(['00', '00', '00', '00']);
+        return;
+      } else {
+        this.render(['00', '00', '00', '00']);
+        return;
+      }
+    }
+    this.enableBtnState(this.startBtn);
+    const timerTime = ClockFace.convertMsToTime(time - Date.now());
+    this.render(timerTime);
+  }
+
+  changeTimer(time) {
+    if (time <= Date.now()) {
+      this.disableBtnState(refs.startBtn);
+
+      this.timerIsActive = false;
+      Notify.success('Timer is over');
+      clearInterval(this.intervalId);
+      return;
+    }
+    this.enableBtnState(refs.startBtn);
+    const timerTime = ClockFace.convertMsToTime(time - Date.now());
+    this.render(timerTime);
+  }
 }
 
-
-
-let intervalId;
-let timerData;
-let timerIsActive = false;
-
-flatpickr(refs.input, optionsToFlatpickr);
-disableBtnState(refs.startBtn);
+const clockFace = new ClockFace(renderTime, refs.startBtn);
+clockFace.disableBtnState(refs.startBtn);
 
 refs.startBtn.addEventListener('click', () => {
-  if (timerIsActive) {
-    return;
-  }
-  intervalId = setInterval(() => {
-    changeTimer(timerData);
-  }, 1000);
-  timerIsActive = true;
+  clockFace.start();
 });
 
-function changeTimer(time) {
-  if (time <= Date.now()) {
-    disableBtnState(refs.startBtn);
-    if (!timerIsActive) {
-      Notify.info('Please choose a date in the future');
-    } else {
-        timerIsActive = false;
-        Notify.success('Timer is over');
-      }
-      
-    clearInterval(intervalId);
-    return;
-  }
-  enableBtnState(refs.startBtn);
-  const timerTime = convertMsToTime(time - Date.now());
-  renderTime(timerTime);
-}
+flatpickr(refs.input, optionsToFlatpickr);
 
 function renderTime(time) {
   [...refs.clockContainer.children].forEach((elem, index) => {
     elem.firstElementChild.textContent = time[index];
   });
-}
-
-function disableBtnState(activeBtn) {
-  activeBtn.style.pointerEvents = 'none';
-  activeBtn.style.color = 'grey';
-}
-function enableBtnState(activeBtn) {
-  activeBtn.style.pointerEvents = 'auto';
-  activeBtn.style.color = 'black';
-}
-
-function convertMsToTime(milliseconds) {
-  let seconds = Math.floor(milliseconds / 1000);
-  let minutes = Math.floor(seconds / 60);
-  let hours = Math.floor(minutes / 60);
-  let day = Math.floor(hours / 24);
-
-  seconds = padTo2Digits(seconds % 60);
-  minutes = padTo2Digits(minutes % 60);
-  hours = padTo2Digits(hours % 24);
-  day = padTo2Digits(day);
-
-  return [day, hours, minutes, seconds];
-}
-function padTo2Digits(num) {
-  return num.toString().padStart(2, '0');
 }
