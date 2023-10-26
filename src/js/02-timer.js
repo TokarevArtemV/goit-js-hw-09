c173be169bd4aa23edb50d86498d7a8507aa01ab
TokarevArtemV <tokarev.artem.v@gmail.com>
2023-10-26 18:02:40 +0300

    add task-2

---
+++ src/js/02-timer.js
@@ -0,0 +1,86 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import 'flatpickr/dist/themes/material_blue.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+let intervalId;
+let timerData;
+let timerIsActive = false;
+
+refs = {
+  input: document.querySelector('input#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  clockContainer: document.querySelector('.timer'),
+  options: {
+    enableTime: true,
+    dateFormat: 'd-m-Y H:i',
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      timerData = new Date(selectedDates[0]).getTime();
+      changeTimer(timerData);
+    },
+  },
+};
+
+refs.startBtn.addEventListener('click', timer);
+
+flatpickr(refs.input, refs.options);
+
+disableBtnState(refs.startBtn);
+
+function timer() {
+  if (timerIsActive) {
+    return;
+  }
+  intervalId = setInterval(() => {
+    changeTimer(timerData);
+  }, 1000);
+  timerIsActive = true;
+}
+
+function changeTimer(time) {
+  if (time <= Date.now()) {
+    disableBtnState(refs.startBtn);
+    !timerIsActive
+      ? Notify.info('Please choose a date in the future')
+      : ((timerIsActive = false), Notify.success('Timer is over'));
+    clearInterval(intervalId);
+    return;
+  }
+  enableBtnState(refs.startBtn);
+  renderTime(convertMsToTime(time - Date.now()));
+}
+
+function renderTime(time) {
+  [...refs.clockContainer.children].forEach((elem, index) => {
+    elem.firstElementChild.textContent = time[index];
+  });
+}
+
+function disableBtnState(activeBtn) {
+  activeBtn.style.pointerEvents = 'none';
+  activeBtn.style.color = 'grey';
+}
+function enableBtnState(activeBtn) {
+  activeBtn.style.pointerEvents = 'auto';
+  activeBtn.style.color = 'black';
+}
+
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+function convertMsToTime(milliseconds) {
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+  let day = Math.floor(hours / 24);
+
+  seconds = padTo2Digits(seconds % 60);
+  minutes = padTo2Digits(minutes % 60);
+  hours = padTo2Digits(hours % 24);
+  day = padTo2Digits(day);
+
+  return [day, hours, minutes, seconds];
+}
